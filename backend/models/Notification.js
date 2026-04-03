4b2e05289f300c3c290c1ae52fcfc1fe65613de8
unknown <stefanpopovicnew@gmail.com>
2026-04-03 12:20:14 +0200

    added Notification model - all models & relationships among them completed

---
+++ backend/models/Notification.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    // The person who triggered the action (e.g., who added the comment)
+    actor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["task_moved", "comment_added", "member_invited", "task_assigned"],
+        required: true
+    },
+    // Reference to the project where this happened
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true 
+    },
+    task: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+        required: false 
+    },
+    message: {
+        type: String,
+        default: "",
+        maxLength: [200, "Message must have less than 200 characters."]
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+// Indexing for performance
+// Since you'll always be looking for "unread notifications for User X"
+notificationSchema.index({ recipient: 1, isRead: 1 });
+
+module.exports = mongoose.model("Notification", notificationSchema);
