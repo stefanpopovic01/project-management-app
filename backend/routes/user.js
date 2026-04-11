a9aa963ca7426622ac13404a80dcb427c5ef661d
unknown <stefanpopovicnew@gmail.com>
2026-04-11 17:54:55 +0200

    feat(user) added middleware inside router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,14 +5,11 @@ const { auth, isAccountOwner } = require("../middleware/authMiddleware");
 
 router.get("/", auth, userController.getUsers);
 router.get("/:id", auth, userController.getUser);
-router.patch("/:id", auth, isAccountOwner, userController.editUser);
-router.delete("/:id", auth, isAccountOwner, userController.deleteUser);
-
-
 router.get("/followers/:id", auth, userController.getFollowers);
 router.get("/following/:id", auth, userController.getFollowing);
+router.patch("/:id", auth, isAccountOwner, userController.editUser);
 router.patch("/follow/:id", auth, userController.follow);
 router.patch("/unfollow/:id", auth, userController.unfollow);
-
+router.delete("/:id", auth, isAccountOwner, userController.deleteUser);
 
 module.exports = router;
