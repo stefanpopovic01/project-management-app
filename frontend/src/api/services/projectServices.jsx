68e02a0ff7aeb7ad1355cd2e62d67c6dbe13aa60
unknown <stefanpopovicnew@gmail.com>
2026-04-24 22:17:14 +0200

    refractor(api) support query params in projectServices

--- frontend/src/api/services/projectServices.jsx
+++ frontend/src/api/services/projectServices.jsx
@@ -1,9 +1,8 @@
 import api from "../axios";
 
-export const getUserProjects = (id) => {
-    return api.get(`/project/user/${id}`);
-}
-
-export const getAssignedProjects = (id) => {
-    return api.get(`/project/assigned/${id}`);
-}
+export const getUserProjects = (id, search = "") => {
+    return api.get(`/project/user/${id}?search=${search}`);
+};
+export const getAssignedProjects = (id, search = "") => {
+    return api.get(`/project/assigned/${id}?search=${search}`);
+};
